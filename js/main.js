9fb76461c412d1f10585ed0e194ab11fdd262f0c
gena granatov <102759762+granatov@users.noreply.github.com>
2022-06-03 01:46:56 +0500

    add scroll to ID

--- js/main.js
+++ js/main.js
@@ -9,11 +9,32 @@ $(function () {
         breakpoint: 481,
         settings: {
           arrows: false,
+          autoplay: true,
+          autoplaySpeed: 2000,
         },
       },
     ],
   });
   $(".menu__btn").on("click", function () {
     $(".menu__btn, .menu__list").toggleClass("active");
+  });
+});
+
+$(document).ready(function () {
+  $("#menu").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+
+    var id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+
+    $("body,html").animate({ scrollTop: top }, 1500);
   });
 });
